caa05c3183b1b114d4566a08d8c9ce7b371ca836
Robert Davidson <davidsonrobert1996@gmail.com>
2020-02-12 14:51:09 -0600

    make it great again

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "password",
+        password: "banker",
         database: "burgers_db"
     });
 }
